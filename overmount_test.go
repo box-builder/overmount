ddf9e6c63e77b8681cfc1be7d3f5ace858057939
Erik Hollensbe <github@hollensbe.org>
2017-03-13 04:55:13 -0700

    Flesh out layer mount test
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- overmount_test.go
+++ overmount_test.go
@@ -48,9 +48,31 @@ func (m *mountSuite) TestRepositoryTempDir(c *C) {
 	c.Assert(first, Equals, tmpdirBase+"/")
 }
 
-func (m *mountSuite) TestMount(c *C) {
+func (m *mountSuite) TestBasicLayerMount(c *C) {
 	l := m.Repository.NewLayer("one", nil, AssetNil(struct{}{}))
 	mount, err := l.Mount()
 	c.Assert(err, IsNil)
+	c.Assert(mount.Lower, Equals, mount.Upper)
 	c.Assert(mount.Mounted(), Equals, true)
+	c.Assert(l.ID, Equals, "one")
+	c.Assert(l.Mounted(), Equals, true)
+	l2 := m.Repository.NewLayer("two", l, AssetNil(struct{}{}))
+	c.Assert(l2.Parent, DeepEquals, l)
+	mount2, err := l2.Mount()
+	c.Assert(err, IsNil)
+	c.Assert(mount2.Mounted(), Equals, true)
+	c.Assert(l2.Mounted(), Equals, true)
+	c.Assert(mount2.Lower, Equals, mount.Target)
+	c.Assert(mount2.Lower, Not(Equals), mount2.Upper)
+	c.Assert(mount2.Target, Not(Equals), mount.Target)
+	c.Assert(mount2.Target, Not(Equals), mount2.Upper)
+	c.Assert(ioutil.WriteFile(path.Join(mount2.Target, "test"), nil, 0644), IsNil)
+	_, err = os.Stat(path.Join(mount2.Upper, "test"))
+	c.Assert(err, IsNil)
+	_, err = os.Stat(path.Join(mount2.Lower, "test"))
+	c.Assert(err, NotNil)
+	_, err = os.Stat(path.Join(mount.Target, "test"))
+	c.Assert(err, NotNil)
+	_, err = os.Stat(path.Join(mount.Upper, "test"))
+	c.Assert(err, NotNil)
 }
