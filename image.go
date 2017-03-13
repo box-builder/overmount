ff1107fb3a4aab98343f17a789515297e318d4c1
Erik Hollensbe <github@hollensbe.org>
2017-03-13 06:04:00 -0700

    ok i suck and it's time to move to working in branches
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- image.go
+++ image.go
@@ -14,7 +14,8 @@ func (i *Image) Mount() error {
 	return nil
 }
 
-// Unmount an image's layers in order, returns an error on an interruption
+// Unmount an image's layers in order, returns an error on an interruption.
+// Can safely be retried.
 func (i *Image) Unmount() error {
 	for x := len(i.mounts) - 1; x >= 0; x-- {
 		if err := i.mounts[x].Close(); err != nil {
@@ -23,8 +24,6 @@ func (i *Image) Unmount() error {
 
 		i.mounts = i.mounts[:x]
 	}
-
-	i.mounts = []*Mount{} // clear the last one
 
 	return nil
 }
