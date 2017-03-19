18705c114bfef5678531cb197b48a6044c466c33
Erik Hollensbe <github@hollensbe.org>
2017-03-18 20:01:27 -0700

    installer update
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- install_box.sh
+++ install_box.sh
@@ -5,5 +5,5 @@ set -e
 if [ "x$(which box)" = "x" ]
 then
   echo "Installing erikh/box to build docker images; may require sudo password."
-  curl -sSL https://raw.githubusercontent.com/erikh/box/master/install.sh | sudo bash
+  curl -sSL box-builder.sh | sudo bash
 fi
