c2800c79e186a91aa38270a0be2c6e6bf6020169
Erik Hollensbe <github@hollensbe.org>
2017-03-22 06:44:12 -0700

    repository.go: repository-level locks for layer/mount CRUD ops
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- repository.go
+++ repository.go
@@ -3,12 +3,17 @@ package overmount
 import (
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 	"sync"
 
+	"golang.org/x/sys/unix"
+
 	"github.com/pkg/errors"
 )
+
+const lockFile = "repository.lock"
 
 // NewRepository constructs a *Repository and creates the dir in which the
 // repository lives. A repository is used to hold images and mounts.
@@ -67,9 +72,29 @@ func (r *Repository) mkdirCheckRel(path string) error {
 	return os.MkdirAll(path, 0700)
 }
 
-func (r *Repository) edit(editFunc func() error) error {
+func (r *Repository) edit(editFunc func() error) (retErr error) {
 	r.editMutex.Lock()
 	defer r.editMutex.Unlock()
+
+	f, err := os.Create(path.Join(r.baseDir, lockFile))
+	if err != nil {
+		return err
+	}
+
+	if err := unix.Flock(int(f.Fd()), unix.LOCK_EX|unix.LOCK_NB); err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := unix.Flock(int(f.Fd()), unix.LOCK_UN); err != nil {
+			retErr = err
+		}
+		f.Close()
+		if err := os.Remove(f.Name()); err != nil {
+			retErr = errors.Wrap(retErr, err.Error())
+		}
+	}()
+
 	return editFunc()
 }
 
