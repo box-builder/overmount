2900e1644ac582f3965363877cfc769c42043d3d
Erik Hollensbe <github@hollensbe.org>
2017-03-24 04:11:55 -0700

    Updated example to use new Import call
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- examples/pull/main.go
+++ examples/pull/main.go
@@ -7,18 +7,16 @@
 package main
 
 import (
-	"archive/tar"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-	"path"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/erikh/overmount"
+	"github.com/erikh/overmount/imgio"
 )
 
 func main() {
@@ -38,6 +36,8 @@ func main() {
 			panic(err)
 		}
 	}
+
+	fmt.Println("repository:", tmpdir)
 
 	repo, err := overmount.NewRepository(tmpdir)
 	if err != nil {
@@ -63,82 +63,17 @@ func main() {
 		panic(err)
 	}
 
-	layerMap := map[string]string{}
-	var manifest []map[string]interface{}
-
-	tr := tar.NewReader(reader)
-
-	tmpdir2, err := ioutil.TempDir("", "")
+	docker, err := imgio.NewDocker(dockerClient)
 	if err != nil {
 		panic(err)
 	}
 
-	for {
-		header, err := tr.Next()
-		if err != nil {
-			break
-		}
-		if path.Base(header.Name) == "layer.tar" {
-			layerID := path.Base(path.Dir(header.Name))
-			f, err := os.Create(path.Join(tmpdir2, layerID+".tar"))
-			if err != nil {
-				panic(err)
-			}
-			if _, err := io.Copy(f, tr); err != nil {
-				panic(err)
-			}
-			f.Close()
-			layerMap[layerID] = f.Name()
-		} else if path.Base(header.Name) == "manifest.json" {
-			content, err := ioutil.ReadAll(tr)
-			if err != nil {
-				panic(err)
-			}
-			if err := json.Unmarshal(content, &manifest); err != nil {
-				panic(err)
-			}
-		} else {
-			io.Copy(ioutil.Discard, tr)
-		}
-	}
-	reader.Close()
-
-	var parent *overmount.Layer // at the end of the loop, the parent will be the top-most layer
-
-	for _, tmp := range manifest[0]["Layers"].([]interface{}) {
-		layerID := path.Dir(tmp.(string))
-		tarfile, err := os.Open(layerMap[layerID])
-		if err != nil {
-			panic(err)
-		}
-		layer, err := repo.CreateLayer(layerID, parent)
-		if err != nil {
-			if os.IsExist(err) {
-				continue
-			}
-			panic(err)
-		}
-		parent = layer
-		digest, err := layer.Unpack(tarfile)
-		if err != nil {
-			if os.IsExist(err) {
-				continue
-			}
-			panic(err)
-		}
-
-		fmt.Printf("unpacked layer with digest %q (id: %v) to %v\n", digest[7:19], layer.ID()[:12], layer.Path())
-	}
-
-	image := repo.NewImage(parent)
-	if err := image.Mount(); err != nil {
+	layers, err := repo.Import(docker, reader)
+	if err != nil {
 		panic(err)
 	}
 
-	// Saves all the parent references to the repository for future lookups.
-	if err := image.Commit(); err != nil {
-		panic(err)
+	for _, layer := range layers {
+		fmt.Println(layer.ID())
 	}
-
-	fmt.Printf("Mount is at %v\n", parent.MountPath())
 }
