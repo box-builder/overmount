dac8678f557475297f3278199f030d850ca1ab23
Erik Hollensbe <github@hollensbe.org>
2017-03-24 15:14:45 -0700

    imgio: significant refactor of docker export routines
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- imgio/docker_export.go
+++ imgio/docker_export.go
@@ -20,192 +20,228 @@ func (d *Docker) Export(layer *om.Layer) (io.ReadCloser, error) {
 		return nil, errors.Wrap(om.ErrInvalidLayer, "layer does not exist")
 	}
 
+	r, w := io.Pipe()
+	go writeTar(layer, w)
+
+	return r, nil
+}
+
+func writeTar(layer *om.Layer, w *io.PipeWriter) (retErr error) {
+	defer func() {
+		if retErr == nil {
+			w.Close()
+		} else {
+			w.CloseWithError(retErr)
+		}
+	}()
+
+	tw := tar.NewWriter(w)
+	layerIDs := []string{}
+
+	for iter := layer; iter != nil; iter = iter.Parent {
+		layerIDs = append(layerIDs, iter.ID())
+
+		if err := writeIDDir(iter, tw); err != nil {
+			return err
+		}
+
+		if err := packLayer(iter, tw); err != nil {
+			return err
+		}
+
+		if err := writeLayerConfig(iter, tw); err != nil {
+			return err
+		}
+	}
+
+	if err := writeRepositories(tw); err != nil {
+		return err
+	}
+
+	if err := writeManifest(layer, layerIDs, tw); err != nil {
+		return err
+	}
+
+	if err := writeImageConfig(layer, tw); err != nil {
+		return err
+	}
+
+	if err := tw.Close(); err != nil {
+		return err
+	}
+
+	w.Close()
+
+	return nil
+}
+
+func writeIDDir(iter *om.Layer, tw *tar.Writer) error {
+	err := tw.WriteHeader(&tar.Header{
+		Name:     iter.ID(),
+		Mode:     0700,
+		Typeflag: tar.TypeDir,
+	})
+	if err != nil {
+		return errors.Wrap(om.ErrImageCannotBeComposed, "cannot add directory to tar writer")
+	}
+
+	return nil
+}
+
+func packLayer(iter *om.Layer, tw *tar.Writer) error {
+	tf, err := ioutil.TempFile("", "layer-")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		tf.Close()
+		os.Remove(tf.Name())
+	}()
+
+	if _, err := iter.Pack(tf); err != nil {
+		return err
+	}
+
+	if _, err := tf.Seek(0, 0); err != nil {
+		return err
+	}
+
+	fi, err := tf.Stat()
+	if err != nil {
+		return err
+	}
+
+	err = tw.WriteHeader(&tar.Header{
+		Name:     path.Join(iter.ID(), "layer.tar"),
+		Mode:     0600,
+		Typeflag: tar.TypeReg,
+		Size:     fi.Size(),
+	})
+
+	if err != nil {
+		return errors.Wrap(om.ErrImageCannotBeComposed, "cannot add file to tar writer")
+	}
+
+	if _, err := io.Copy(tw, tf); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeLayerConfig(iter *om.Layer, tw *tar.Writer) error {
+	var parent string
+	if iter.Parent != nil {
+		parent = iter.Parent.ID()
+	}
+
+	content, err := json.Marshal(map[string]interface{}{
+		"id":     iter.ID(),
+		"parent": parent,
+		"config": v1.ImageConfig{},
+	})
+	if err != nil {
+		return err
+	}
+
+	err = tw.WriteHeader(&tar.Header{
+		Name:     path.Join(iter.ID(), "json"),
+		Mode:     0600,
+		Typeflag: tar.TypeReg,
+		Size:     int64(len(content)),
+	})
+	if err != nil {
+		return err
+	}
+
+	if _, err := tw.Write(content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeRepositories(tw *tar.Writer) error {
+	content, err := json.Marshal(map[string]interface{}{})
+	if err != nil {
+		return err
+	}
+
+	err = tw.WriteHeader(&tar.Header{
+		Name:     "repositories",
+		Mode:     0600,
+		Typeflag: tar.TypeReg,
+		Size:     int64(len(content)),
+	})
+	if err != nil {
+		return err
+	}
+
+	if _, err := tw.Write(content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeManifest(layer *om.Layer, layerIDs []string, tw *tar.Writer) error {
+	content, err := json.Marshal([]map[string]interface{}{
+		{
+			"Config":   fmt.Sprintf("%s.json", layer.ID()),
+			"RepoTags": []string{},
+			"Layers":   layerIDs,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	err = tw.WriteHeader(&tar.Header{
+		Name:     "manifest.json",
+		Mode:     0600,
+		Typeflag: tar.TypeReg,
+		Size:     int64(len(content)),
+	})
+	if err != nil {
+		return err
+	}
+
+	if _, err := tw.Write(content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeImageConfig(layer *om.Layer, tw *tar.Writer) error {
 	config, err := layer.Config()
 	if err != nil {
-		return nil, errors.Wrap(om.ErrInvalidLayer, err.Error())
+		return errors.Wrap(om.ErrInvalidLayer, err.Error())
 	}
 
 	if config == nil {
-		return nil, errors.Wrap(om.ErrImageCannotBeComposed, "missing image configuration")
+		return errors.Wrap(om.ErrImageCannotBeComposed, "missing image configuration")
 	}
 
-	r, w := io.Pipe()
+	content, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
 
-	go func(layer *om.Layer, w *io.PipeWriter) {
-		tw := tar.NewWriter(w)
-		files := []*os.File{}
-		defer func() {
-			for _, file := range files {
-				file.Close()
-				os.Remove(file.Name())
-			}
-		}()
+	err = tw.WriteHeader(&tar.Header{
+		Name:     fmt.Sprintf("%s.json", layer.ID()),
+		Mode:     0600,
+		Typeflag: tar.TypeReg,
+		Size:     int64(len(content)),
+	})
+	if err != nil {
+		return err
+	}
 
-		layerIDs := []string{}
+	if _, err := tw.Write(content); err != nil {
+		return err
+	}
 
-		for iter := layer; iter != nil; iter = iter.Parent {
-			layerIDs = append(layerIDs, iter.ID())
-
-			err := tw.WriteHeader(&tar.Header{
-				Name:     iter.ID(),
-				Mode:     0700,
-				Typeflag: tar.TypeDir,
-			})
-			if err != nil {
-				w.CloseWithError(errors.Wrap(om.ErrImageCannotBeComposed, "cannot add directory to tar writer"))
-				return
-			}
-
-			tf, err := ioutil.TempFile("", "layer-")
-			if err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			files = append(files, tf)
-
-			if _, err := iter.Pack(tf); err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			if _, err := tf.Seek(0, 0); err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			fi, err := tf.Stat()
-			if err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			var parent string
-			if iter.Parent != nil {
-				parent = iter.Parent.ID()
-			}
-
-			content, err := json.Marshal(map[string]interface{}{
-				"id":     iter.ID(),
-				"parent": parent,
-				"config": v1.ImageConfig{},
-			})
-			if err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			err = tw.WriteHeader(&tar.Header{
-				Name:     path.Join(iter.ID(), "json"),
-				Mode:     0600,
-				Typeflag: tar.TypeReg,
-				Size:     int64(len(content)),
-			})
-			if err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			if _, err := tw.Write(content); err != nil {
-				w.CloseWithError(err)
-				return
-			}
-
-			err = tw.WriteHeader(&tar.Header{
-				Name:     path.Join(iter.ID(), "layer.tar"),
-				Mode:     0600,
-				Typeflag: tar.TypeReg,
-				Size:     fi.Size(),
-			})
-
-			if err != nil {
-				w.CloseWithError(errors.Wrap(om.ErrImageCannotBeComposed, "cannot add file to tar writer"))
-				return
-			}
-
-			if _, err := io.Copy(tw, tf); err != nil {
-				w.CloseWithError(err)
-				return
-			}
-		}
-
-		content, err := json.Marshal(map[string]interface{}{})
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		err = tw.WriteHeader(&tar.Header{
-			Name:     "repositories",
-			Mode:     0600,
-			Typeflag: tar.TypeReg,
-			Size:     int64(len(content)),
-		})
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		if _, err := tw.Write(content); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		content, err = json.Marshal([]map[string]interface{}{
-			{
-				"Config":   fmt.Sprintf("%s.json", layer.ID()),
-				"RepoTags": []string{},
-				"Layers":   layerIDs,
-			},
-		})
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		err = tw.WriteHeader(&tar.Header{
-			Name:     "manifest.json",
-			Mode:     0600,
-			Typeflag: tar.TypeReg,
-			Size:     int64(len(content)),
-		})
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		if _, err := tw.Write(content); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-		content, err = json.Marshal(config)
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		err = tw.WriteHeader(&tar.Header{
-			Name:     fmt.Sprintf("%s.json", layer.ID()),
-			Mode:     0600,
-			Typeflag: tar.TypeReg,
-			Size:     int64(len(content)),
-		})
-		if err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		if _, err := tw.Write(content); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-
-		if err := tw.Close(); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-		w.Close()
-	}(layer, w)
-
-	return r, nil
+	return nil
 }
