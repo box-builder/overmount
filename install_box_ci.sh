2d683c184fcc306f0f464c570871af9861339b3b
Erik Hollensbe <github@hollensbe.org>
2017-03-25 04:59:11 -0700

    Update to use box-builder/box
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- install_box_ci.sh
+++ install_box_ci.sh
@@ -5,6 +5,6 @@ set -e
 if [ ! -x bin/box ]
 then
   mkdir -p bin
-  curl -sSL https://github.com/erikh/box/releases/download/v0.5.1/box-0.5.1.linux.gz | gzip -dc >bin/box
+  curl -sSL https://github.com/box-builder/box/releases/download/v0.5.1/box-0.5.1.linux.gz | gzip -dc >bin/box
   chmod +x bin/box
 fi
