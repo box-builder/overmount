d8f8a5c3a9b332bb7c23036fd6d61ca4048fa008
Erik Hollensbe <github@hollensbe.org>
2017-04-11 09:36:11 -0700

    layer.go,layer_test.go: CreateLayerFromAsset create layers with IDs based on the asset digest.
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- util_test.go
+++ util_test.go
@@ -14,7 +14,7 @@ func (m *mountSuite) makeImage(c *C, layerCount int) (*Image, *Layer) {
 	var parent *Layer
 	for i := 0; i < layerCount; i++ {
 		var err error
-		parent, err = m.Repository.CreateLayer(fmt.Sprintf("test%d", i), parent)
+		parent, err = m.Repository.CreateLayer(fmt.Sprintf("test%d", i), parent, false)
 		c.Assert(err, IsNil)
 
 		r, w := io.Pipe()
@@ -41,6 +41,7 @@ func (m *mountSuite) makeImage(c *C, layerCount int) (*Image, *Layer) {
 			return nil
 		})
 
+		w.Close()
 		tw.Close()
 	}
 	return m.Repository.NewImage(parent), parent
