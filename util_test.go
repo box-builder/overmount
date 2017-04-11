f10e0453d45049762049c8b1b2bf60252df646f4
Erik Hollensbe <github@hollensbe.org>
2017-04-11 09:35:35 -0700

    util_test.go: tar the overmount repository each layer.
    This gives us real-world data to work with.
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- util_test.go
+++ util_test.go
@@ -1,7 +1,11 @@
 package overmount
 
 import (
+	"archive/tar"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 
 	. "gopkg.in/check.v1"
 )
@@ -12,6 +16,32 @@ func (m *mountSuite) makeImage(c *C, layerCount int) (*Image, *Layer) {
 		var err error
 		parent, err = m.Repository.CreateLayer(fmt.Sprintf("test%d", i), parent)
 		c.Assert(err, IsNil)
+
+		r, w := io.Pipe()
+		go parent.Unpack(r)
+
+		tw := tar.NewWriter(w)
+
+		wd, err := os.Getwd()
+		c.Assert(err, IsNil)
+
+		filepath.Walk(wd, func(p string, fi os.FileInfo, err error) error {
+			header, err := tar.FileInfoHeader(fi, fi.Name())
+			c.Assert(err, IsNil)
+			c.Assert(tw.WriteHeader(header), IsNil)
+
+			if !fi.IsDir() {
+				f, err := os.Open(p)
+				c.Assert(err, IsNil)
+				_, err = io.Copy(tw, f)
+				c.Assert(err, IsNil)
+				return f.Close()
+			}
+
+			return nil
+		})
+
+		tw.Close()
 	}
 	return m.Repository.NewImage(parent), parent
 }
